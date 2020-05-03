b412acb36ecbd08a779c7f58d6fb27de4b1dbed7
ewami <efili1989@gmail.com>
2020-05-03 16:02:34 +0200

    post page css changes

--- src/PostPage.js
+++ src/PostPage.js
@@ -9,7 +9,7 @@ export default function PostPage(props) {
         <Link to="/">Home</Link>
       </div>
       <div className="card shadow-sm mb-4">
-        <div className="card-body pb-0">
+        <div className="card-body pb-0 postBody">
           <h6 className="card-subtitle mb-3">{props.date}</h6>
           <div>
             <h1 className="title">{props.title}</h1>
@@ -17,7 +17,7 @@ export default function PostPage(props) {
             <div>{props.lead}</div>
             <br></br>
             <div className="img_div">
-              <img className="card__image" src={props.image} />
+              <img className="card__image postImg" src={props.image} />
             </div>
             <br></br>
             <div>{props.full}</div>
