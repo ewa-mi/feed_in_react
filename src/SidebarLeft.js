4f8cf4743b6e78a5cb105dff1b8b5e803b840186
ewami <efili1989@gmail.com>
2020-05-04 09:24:57 +0200

    iframe, recognizing media type from API (left sidebar)

--- src/SidebarLeft.js
+++ src/SidebarLeft.js
@@ -4,33 +4,40 @@ import "./SidebarLeft.css";
 
 export default function SidebarLeft() {
   const [data, setData] = useState([]);
+  const [mediaType, setMediaType] = useState("");
 
   useEffect(() => {
-    // console.log("Use Effect!");
     const fetchData = async () => {
       const response = await axios.get(
         "https://api.nasa.gov/planetary/apod?api_key=esGhqpCdz7yQx2f3DAwcWxf412oKlvvY8cdL3MAr"
       );
-      // console.log("axios response", response.data);
+      setMediaType(response.data.media_type);
       setData(response.data);
     };
 
     fetchData();
   }, []);
 
-  // console.log(data);
-
   return (
     <div className="card shadow-sm mb-4">
       <div className="card-body pb-0">
-        <h5 className="card-title">NASA daily image</h5>
+        <h5 className="card-title">NASA daily picture</h5>
         <h6 className="card-subtitle mb-3">{data.date}</h6>
         <div>
           {data.title}
           <br />
           <br />
           <div>
-            <img className="card__image" src={data.hdurl} />
+            {mediaType === "video" ? (
+              <iframe
+                src={data.url}
+                frameBorder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            ) : (
+              <img className="card__image" src={data.hdurl} />
+            )}
           </div>
         </div>
       </div>
